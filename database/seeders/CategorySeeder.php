95be607d102d00fe9f78819b5404528a4a2d4116
giovannilampis <giolampis@hotmail.com>
2023-06-22 22:33:09 +0200

    add_category_id_to_projects_table

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Admin\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -22,6 +23,16 @@ class CategorySeeder extends Seeder
             'Design'
         ];
 
-        $new_cat = new Category();
+        foreach( $categories as $elem ){
+
+            $new_cat = new Category();
+
+            $new_cat->name = $elem;
+
+            $new_cat->slug = Str::slug($new_cat->name);
+
+            $new_cat->save();
+        }
+
     }
 }
