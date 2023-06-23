0d6e81969b5860516d1ec8a7feb5556161ff4093
giovannilampis <giolampis@hotmail.com>
2023-06-23 17:26:21 +0200

    push change in show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -9,7 +9,7 @@
         <div class="col-6 mt-5">
 
             <div class="card">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="{{ asset('storage/'.$project->img_url) }}" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h4 class="card-title"><span 
                     class="badge rounded-pill bg-primary">{{$project->category->name?? 'No Category' }}</span>
